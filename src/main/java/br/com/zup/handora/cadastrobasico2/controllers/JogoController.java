31cbab3edc50068ce8721d1a49e3236b5fc3de70
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-28 10:28:09 -0300

    Inject JogoRepository dependency in JogoController

--- src/main/java/br/com/zup/handora/cadastrobasico2/controllers/JogoController.java
+++ src/main/java/br/com/zup/handora/cadastrobasico2/controllers/JogoController.java
@@ -3,10 +3,18 @@ package br.com.zup.handora.cadastrobasico2.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.zup.handora.cadastrobasico2.repositories.JogoRepository;
+
 @RestController
 @RequestMapping(JogoController.BASE_URI)
 public class JogoController {
 
     public final static String BASE_URI = "/jogos";
+
+    private final JogoRepository jogoRepository;
+
+    public JogoController(JogoRepository jogoRepository) {
+        this.jogoRepository = jogoRepository;
+    }
 
 }

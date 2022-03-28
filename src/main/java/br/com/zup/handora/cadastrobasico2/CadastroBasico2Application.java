f3c6abfb21c1be13443ac989c03591db5de20c2e
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-28 10:10:54 -0300

    Initial commit

---
+++ src/main/java/br/com/zup/handora/cadastrobasico2/CadastroBasico2Application.java
@@ -0,0 +1,13 @@
+package br.com.zup.handora.cadastrobasico2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroBasico2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroBasico2Application.class, args);
+	}
+
+}

c53f9e0c2e73565bf050a50839bb2e3ca888daa9
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-28 10:42:27 -0300

    Add Jogo creation method to JogoController

--- src/main/java/br/com/zup/handora/cadastrobasico2/controllers/JogoController.java
+++ src/main/java/br/com/zup/handora/cadastrobasico2/controllers/JogoController.java
@@ -1,8 +1,18 @@
 package br.com.zup.handora.cadastrobasico2.controllers;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.zup.handora.cadastrobasico2.models.Jogo;
+import br.com.zup.handora.cadastrobasico2.models.JogoDTO;
 import br.com.zup.handora.cadastrobasico2.repositories.JogoRepository;
 
 @RestController
@@ -15,6 +25,18 @@ public class JogoController {
 
     public JogoController(JogoRepository jogoRepository) {
         this.jogoRepository = jogoRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> postMethodName(@RequestBody @Valid JogoDTO jogoDTO,
+                                               UriComponentsBuilder uriComponentsBuilder) {
+        Jogo jogo = jogoRepository.save(jogoDTO.paraJogo());
+
+        URI location = uriComponentsBuilder.path(BASE_URI + "/{id}")
+                                           .buildAndExpand(jogo.getId())
+                                           .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }

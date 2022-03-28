7ec96d1ba2de964eaaa9bdc52635e2855ea05bcf
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-28 10:26:53 -0300

    Add JogoRepository

---
+++ src/main/java/br/com/zup/handora/cadastrobasico2/repositories/JogoRepository.java
@@ -0,0 +1,9 @@
+package br.com.zup.handora.cadastrobasico2.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.zup.handora.cadastrobasico2.models.Jogo;
+
+public interface JogoRepository extends JpaRepository<Jogo, Long> {
+
+}

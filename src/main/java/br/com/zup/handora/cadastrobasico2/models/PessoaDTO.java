f959c05588c9e7b6cc3f155de5d16367b79961c6
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-08 01:48:30 -0300

    Add conversion method to Pessoa DTO

--- src/main/java/br/com/zup/handora/cadastrobasico2/models/PessoaDTO.java
+++ src/main/java/br/com/zup/handora/cadastrobasico2/models/PessoaDTO.java
@@ -1,9 +1,15 @@
 package br.com.zup.handora.cadastrobasico2.models;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import br.com.zup.handora.cadastrobasico2.repositories.JogoRepository;
 
 public class PessoaDTO {
 
@@ -18,6 +24,25 @@ public class PessoaDTO {
     public PessoaDTO(@NotBlank String nome, Set<Long> jogoIds) {
         this.nome = nome;
         this.jogoIds = jogoIds;
+    }
+
+    public Pessoa toModel(JogoRepository jogoRepository) {
+        Pessoa pessoa = new Pessoa(nome);
+        Set<Jogo> novosJogos = jogoIds.stream()
+                                      .map(
+                                          id -> jogoRepository.findById(id)
+                                                              .orElseThrow(
+                                                                  () -> new ResponseStatusException(
+                                                                      HttpStatus.NOT_FOUND,
+                                                                      "Não existe um jogo com o ID informado."
+                                                                  )
+                                                              )
+                                      )
+                                      .collect(Collectors.toSet());
+        pessoa.adicionar(novosJogos);
+
+        return pessoa;
+
     }
 
     public String getNome() {
